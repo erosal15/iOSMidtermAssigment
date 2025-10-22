32b5f48eec28a03a053e005dc52020a8704f2745
Edgar Rosales <erosal15@my.smccd.edu>
2025-10-21 19:48:05 -0700

    Initial Commit

---
+++ midterm/midtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  midtermApp.swift
+//  midterm
+//
+//  Created by cisstudent on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct midtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ midtermTests/midtermTests.swift
@@ -0,0 +1,16 @@
+//
+//  midtermTests.swift
+//  midtermTests
+//
+//  Created by cisstudent on 10/21/25.
+//
+
+import Testing
+
+struct midtermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
